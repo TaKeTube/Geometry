293e52dae8006c4689395be3aefa8e793402d896
TaKe Tube <jamie.zimu.guan@gmail.com>
2021-06-09 22:17:54 +0800

    5 vertexes selected test

--- Refinement/src/RefineTest.cpp
+++ Refinement/src/RefineTest.cpp
@@ -8,8 +8,9 @@
 
 // #define TEST_1_VERT
 // #define TEST_2_VERT
-#define TEST_3_VERT
-#define TEST_4_VERT
+// #define TEST_3_VERT
+// #define TEST_4_VERT
+#define TEST_5_VERT
 
 void refine1vertTest(int i, char* test_file){
     std::string idx;
@@ -113,6 +114,37 @@ void refine4vertTest(int i, int j, int k, int n, char* test_file){
     vtkWriter((std::string("v")+iIdx+jIdx+kIdx+nIdx+std::string(".vtk")).c_str(), mesh);
 }
 
+void refine5vertTest(int i, int j, int k, int n, int m, char* test_file){
+    /* test 3 vertexes selected */
+    Mesh mesh = Mesh();
+    std::string iIdx, jIdx, kIdx, nIdx, mIdx;
+
+    /* select 2 target vertexes */
+    std::vector<size_t> selectedV;
+    selectedV.push_back(i);
+    selectedV.push_back(j);
+    selectedV.push_back(k);
+    selectedV.push_back(n);
+    selectedV.push_back(m);
+
+    /* read mesh */
+    meshReader(test_file, mesh);
+    
+    /* get vertex - cell pairs */
+    mesh.getVI_CI();
+    
+    /* detect number of flat angles then print it out */
+    mesh.refine(selectedV);
+    
+    /* output the processed mesh */
+    iIdx = '0'+(char)i;
+    jIdx = '0'+(char)j;
+    kIdx = '0'+(char)k;
+    nIdx = '0'+(char)n;
+    mIdx = '0'+(char)m;
+    vtkWriter((std::string("v")+iIdx+jIdx+kIdx+nIdx+mIdx+std::string(".vtk")).c_str(), mesh);
+}
+
 void runTest(){
     char test_file[] = "../data/cube.vtk";
 
@@ -141,5 +173,10 @@ void runTest(){
     refine4vertTest(0, 1, 2, 3, test_file);
     refine4vertTest(4, 5, 6, 7, test_file);
     refine4vertTest(1, 2, 4, 6, test_file);
+    #endif
+
+    #ifdef TEST_5_VERT
+    /* test 5 vertexes selected */
+    refine5vertTest(0, 1, 2, 3, 4, test_file);
     #endif
 }
--- Refinement/src/RefineTest.h
+++ Refinement/src/RefineTest.h
@@ -5,6 +5,7 @@ void refine1vertTest(int i, char* test_file);
 void refine2vertTest(int i, int j, char* test_file);
 void refine3vertTest(int i, int j, int k, char* test_file);
 void refine4vertTest(int i, int j, int k, int n, char* test_file);
+void refine5vertTest(int i, int j, int k, int n, int m, char* test_file);
 void runTest();
 
 #endif
