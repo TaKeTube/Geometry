b45b2196439866b95dad51594822c342e5d32789
TaKe Tube <jamie.zimu.guan@gmail.com>
2021-07-19 07:42:03 +0800

    get surface normal

--- Padding/src/hpMesh.cpp
+++ Padding/src/hpMesh.cpp
@@ -4,6 +4,7 @@
 #include "hpMesh.h"
 
 using namespace std;
+using namespace Eigen;
 
 void Mesh::getGeometryInfo()
 {
@@ -63,5 +64,45 @@ void Mesh::getGeometryInfo()
 
 void Mesh::getSurfaceNormal()
 {
+    for (size_t fIdx = 0; fIdx < F.size(); fIdx++)
+    {
+        if (FinfoMap.at(fIdx).isBoundary)
+        {
+            Face &f = F.at(fIdx);
+            size_t v0Idx = f.at(1), v1Idx = f.at(1), v2Idx = f.at(2), v3Idx = f.at(3);
 
+            Vector3d n0 = getNormal(V.at(v0Idx), V.at(v1Idx), V.at(v2Idx));
+            Vector3d n1 = getNormal(V.at(v2Idx), V.at(v3Idx), V.at(v0Idx));
+
+            if (VinfoMap.find(v1Idx) == VinfoMap.end())
+                VinfoMap[v1Idx].normal = n0;
+            else
+                VinfoMap[v1Idx].normal += n0;
+
+            if (VinfoMap.find(v2Idx) == VinfoMap.end())
+                VinfoMap[v2Idx].normal = n0;
+            else
+                VinfoMap[v2Idx].normal += n0;
+
+            if (VinfoMap.find(v3Idx) == VinfoMap.end())
+                VinfoMap[v3Idx].normal = n0;
+            else
+                VinfoMap[v3Idx].normal += n0;
+
+            if (VinfoMap.find(v2Idx) == VinfoMap.end())
+                VinfoMap[v2Idx].normal = n1;
+            else
+                VinfoMap[v2Idx].normal += n1;
+
+            if (VinfoMap.find(v3Idx) == VinfoMap.end())
+                VinfoMap[v3Idx].normal = n1;
+            else
+                VinfoMap[v3Idx].normal += n1;
+
+            if (VinfoMap.find(v0Idx) == VinfoMap.end())
+                VinfoMap[v0Idx].normal = n1;
+            else
+                VinfoMap[v0Idx].normal += n1;
+        }
+    }
 }
--- Padding/src/hpUtility.hpp
+++ Padding/src/hpUtility.hpp
@@ -1,6 +1,8 @@
 #ifndef HEX_PADDING_UTILITY_H
 #define HEX_PADDING_UTILITY_H
 
+#include <eigen3/Eigen/Eigen>
+
 /* vertexes order in this table ensures that normal vector of faces points into the cell */
 const unsigned int HexFace[6][4] =
 {
@@ -11,5 +13,12 @@ const unsigned int HexFace[6][4] =
     { 3,2,6,7 },
     { 1,5,6,2 },
 };
+
+/* return (v1-v0) x (v2-v0) */
+inline Eigen::Vector3d getNormal(const Eigen::Vector3d &v0, const Eigen::Vector3d &v1, const Eigen::Vector3d &v2){
+    Eigen::Vector3d dv0 = v1 - v0;
+    Eigen::Vector3d dv1 = v2 - v0;
+    return (dv0.cross(dv1)).normalized();
+}
 
 #endif
