f4c9ea9a93baf60c0bd884a080380f94cd61ec1d
TaKe Tube <jamie.zimu.guan@gmail.com>
2021-07-25 19:47:03 +0800

    cluster normal

--- HexPadding/src/hpUtility.hpp
+++ HexPadding/src/hpUtility.hpp
@@ -41,9 +41,45 @@ namespace HexPadding
         return (dv0.cross(dv1)).normalized();
     }
 
-    Eigen::Vector3d getClusteredNormal(const std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > V)
+    Eigen::Vector3d getClusteredNormal(std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > V)
     {
-        return Eigen::Vector3d::Zero();
+        size_t vsize = V.size();
+        std::vector<char> vflag(vsize, 0);
+        bool isClustered = true;
+        while(isClustered)
+        {
+            isClustered = false;
+            for(size_t i = 0; i < vsize; i++)
+            {
+                if(vflag.at(i))
+                    continue;
+                Eigen::Vector3d &v1 = V.at(i);
+                Eigen::Vector3d v = V.at(i);
+                int count = 0;
+                for(size_t j = i+1; j < vsize; j++)
+                {
+                    if(vflag.at(j))
+                        continue;
+                    Eigen::Vector3d &v2 = V.at(j);
+                    if((v1-v2).squaredNorm() < 0.3)
+                    {
+                        v += v2;
+                        count++;
+                        vflag.at(j) = 1;
+                        isClustered = true;
+                    }
+                }
+                if(count)
+                    v1 = (v/count).normalized();
+            }
+        }
+        Eigen::Vector3d vavg = Eigen::Vector3d::Zero();
+        for(size_t i = 0; i < V.size(); i++)
+        {
+            if(!vflag.at(i))
+                vavg += V.at(i);
+        }
+        return vavg.normalized();
     }
 }
 
